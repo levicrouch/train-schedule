e14a0e552a1ea076af28fd87a158a59955e506d9
Levi Crouch <levi.crouch@gmail.com>
2017-12-09 15:34:41 -0800

    capturing form data, writing to database, and clearing out the form values

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-var painTrainLink = "https://www.youtube.com/watch?v=x5dJb2YG7vU&t=1m20s";
-
 ///////////////////////////////////////////////////////////////
 // variables and objects
 ///////////////////////////////////////////////////////////////
+
+// Debug
+var debug = true;
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBUc0WBppyXGEGszjuslJvOFzA90ENsygU",
+    authDomain: "paintrainschedule.firebaseapp.com",
+    databaseURL: "https://paintrainschedule.firebaseio.com",
+    projectId: "paintrainschedule",
+    storageBucket: "paintrainschedule.appspot.com",
+    messagingSenderId: "581050482081"
+};
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+var painTrainLink = "https://www.youtube.com/watch?v=x5dJb2YG7vU&t=1m20s";
 
 // HTML input id's/classes object
 var input = {
     trainName: "#form-train-name",
     destination: "#form-destination",
     firstTrain: "#form-first-train",
-    frequency: "#form-frequency"
-    
+    frequency: "#form-frequency",
+    form: ".form-control"
+
 }
 // HTML output object
 var output = {
@@ -27,16 +45,38 @@ var formData = {
     destination: "",
     firstTrain: "",
     frequency: 0,
-    submitButton: "#submit"
+    submitButton: "#submit",
+    clear: function () {
+        // clears out the data entered in the form
+        $(input.trainName).val('');
+        $(input.destination).val('');
+        $(input.firstTrain).val('');
+        $(input.frequency).val('');
+    }
 }
 // aftter hitting submit, capture user supplied data
-$(formData.submitButton).click(function(){
+$(formData.submitButton).click(function () {
+    // Prevent the buttons default behavior
+    event.preventDefault();
+    // Capture the data entered in the form
     formData.trainName = $(input.trainName).val();
     formData.destination = $(input.destination).val();
     formData.firstTrain = $(input.firstTrain).val();
     formData.frequency = $(input.frequency).val();
-    for (const prop in formData) {
-        
-        console.log(`formData.${prop} = ${formData[prop]}`);
+    if (debug) {
+        for (const prop in formData) {
+
+            console.log(`formData.${prop} = ${formData[prop]}`);
+        }
     }
+
+    // write inputted data to the firebase database
+    database.ref().push({
+        trainName: formData.trainName,
+        destination: formData.destination,
+        firstTrain: formData.firstTrain,
+        frequency: formData.frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    formData.clear();
 });
